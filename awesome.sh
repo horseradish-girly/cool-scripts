3aac04d8f7a1788818ea36daa1cc0b0ae909301f
horseradish-girly <horseradish-girly@mf.me>
2024-10-18 21:34:35 -0400

    Update awesome.sh

--- awesome.sh
+++ awesome.sh
@@ -1,4 +1,4 @@
 echo "You've been hacked!!!"
-read -r -p "You now legally have to give me your daddy's credit card number: "
+read -r -p "You now legally have to give me your daddy's credit card number: " input
 echo "Thank you!! :D"
-echo "*you're fucking broak now*"
+echo "*you're fucking broke now*"
