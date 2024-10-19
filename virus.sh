96cb589ac7a1fa78a81c59c12317618b9ecf19bb
horseradish-girly <horseradish-girly@mf.me>
2024-10-18 21:44:48 -0400

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -1,8 +1,6 @@
-maleware() {
-  echo "You've been hacked!!!"
-  read -r -p "You now legally have to give me your daddy's credit card number: " input
-  echo "Thank you!! :D"
-  echo "*you're fucking broke now*"
-}
-
-maleware
+bash -c '
+echo "You've been hacked!!!"
+read -r -p "You now legally have to give me your daddy's credit card number: "
+echo "Thank you!! :D"
+echo "*you're fucking broke now*"
+'
