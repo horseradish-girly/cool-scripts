f91e9d8ef003766b7c46232a1ada383c992494f7
horseradish-girly <horseradish-girly@mf.me>
2024-10-18 21:40:02 -0400

    Update and rename awesome.sh to virus.sh

--- awesome.sh
+++
@@ -1,4 +0,0 @@
-echo "You've been hacked!!!"
-read -r -p "You now legally have to give me your daddy's credit card number: " input
-echo "Thank you!! :D"
-echo "*you're fucking broke now*"
---
+++ virus.sh
@@ -0,0 +1,8 @@
+maleware() {
+  echo "You've been hacked!!!"
+  read -r -p "You now legally have to give me your daddy's credit card number: " input
+  echo "Thank you!! :D"
+  echo "*you're fucking broke now*"
+}
+
+maleware
